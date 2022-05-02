19e5b8d289c7ad025697bd33c9d8f240325fb714
dipan46 <dipaneducation46@gmail.com>
2022-05-02 19:39:43 +0530

    adding project file

---
+++ Starting/JavaScript/functionCal.js
@@ -0,0 +1,20 @@
+function mul(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function sum(a, b) {
+    return a * b;
+}
+
+function div(a, b) {
+    return a / b;
+}
+
+
+function calculate(a, b, c) {
+    return c(a, b);
+}
